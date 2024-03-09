1bb53b43817c22945b963cf9db6c9d5a84749c61
maayanagar <nagarmaa@post.bgu.ac.il>
2024-03-09 14:50:54 +0200

    added virtual env to git

--- WEB_Project_part_B_Noa_Maayan/app.py
+++ WEB_Project_part_B_Noa_Maayan/app.py
@@ -1,7 +1,16 @@
 from flask import Blueprint, render_template, request, redirect, session, jsonify, Flask
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+# mongodb connection with one of our users
+uri = "mongodb+srv://maayann126:MAAYANMAAYAN123456@cluster0.gdvjyvv.mongodb.net/?retryWrites=true&w=majority"
+# Create a new client and connect to the server
+cluster = MongoClient(uri, server_api=ServerApi('1'))
+iTennis_DB = cluster["ITennis"]
+
 
 app = Flask(__name__)
-# app.secret_key = '1212'
+app.secret_key = '1212'
 
 
 @app.route('/')
